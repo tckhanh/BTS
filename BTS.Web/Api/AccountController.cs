d9da2753393d606d46d0638374376ce9a08c32cc
TCKhanh <tckhanh.p@gmail.com>
2017-08-29 17:40:02 +0700

    Add IdentityAccount

---
+++ BTS.Web/Api/AccountController.cs
@@ -0,0 +1,70 @@
+﻿using BTS.Service;
+using BTS.Web.App_Start;
+using BTS.Web.Infastructure.Core;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace BTS.Web.Api
+{
+    [RoutePrefix("api/account")]
+    public class AccountController : ApiControllerBase
+    {
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;        
+
+        public AccountController(IErrorService errorService, ApplicationUserManager userManager, ApplicationSignInManager signInManager): base(errorService)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.Current.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        //
+        // POST: /Account/Login
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("login")]
+        public async Task<HttpResponseMessage> Login(HttpRequestMessage request, string userName, string password, bool rememberMe)
+        {
+            if (!ModelState.IsValid)
+            {
+                return request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+
+            // This doesn't count login failures towards account lockout
+            // To enable password failures to trigger account lockout, change to shouldLockout: true
+            var result = await SignInManager.PasswordSignInAsync(userName, password, rememberMe, shouldLockout: false);
+            return request.CreateResponse(HttpStatusCode.OK, result);            
+        }
+    }
+}
--- BTS.Web/App_Start/Startup.cs
+++ BTS.Web/App_Start/Startup.cs
@@ -12,16 +12,21 @@ using BTS.Service;
 using System.Web.Mvc;
 using System.Web.Http;
 using Autofac.Integration.WebApi;
+using Microsoft.AspNet.Identity;
+using BTS.Model.Models;
+using System.Web;
+using Microsoft.Owin.Security.DataProtection;
 
 [assembly: OwinStartup(typeof(BTS.Web.App_Start.Startup))]
 
 namespace BTS.Web.App_Start
 {
-    public class Startup
+    public partial class Startup
     {
         public void Configuration(IAppBuilder app)
         {
             // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            ConfigureAuth(app);
             ConfigAutofac(app);
         }
 
@@ -40,11 +45,11 @@ namespace BTS.Web.App_Start
             builder.RegisterType<BTSDbContext>().AsSelf().InstancePerRequest();
 
             //Asp.net Identity
-            //builder.RegisterType<ApplicationUserStore>().As<IUserStore<ApplicationUser>>().InstancePerRequest();
-            //builder.RegisterType<ApplicationUserManager>().AsSelf().InstancePerRequest();
-            //builder.RegisterType<ApplicationSignInManager>().AsSelf().InstancePerRequest();
-            //builder.Register(c => HttpContext.Current.GetOwinContext().Authentication).InstancePerRequest();
-            //builder.Register(c => app.GetDataProtectionProvider()).InstancePerRequest();
+            builder.RegisterType<ApplicationUserStore>().As<IUserStore<ApplicationUser>>().InstancePerRequest();
+            builder.RegisterType<ApplicationUserManager>().AsSelf().InstancePerRequest();
+            builder.RegisterType<ApplicationSignInManager>().AsSelf().InstancePerRequest();
+            builder.Register(c => HttpContext.Current.GetOwinContext().Authentication).InstancePerRequest();
+            builder.Register(c => app.GetDataProtectionProvider()).InstancePerRequest();
 
 
             // Repositories
